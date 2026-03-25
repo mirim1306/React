488b0092c734f416f405ff4bdcfec0c303928ee0
hwan <kimtaehwanlove1@gmail.com>
2026-03-26 08:44:52 +0900

    counter HTML, JS id, class

---
+++ 02_counter/main.js
@@ -0,0 +1,31 @@
+let num=0;
+function plus(){
+    num++;
+    document.getElementById("number").innerText=num;
+}
+// const resultH1=document.getElementsByTagName("h1")[0];
+// const plusButton=document.getElementsByTagName("button");
+// const resltH1=document.querySelectorAll("h1")[0];
+// const plusButton=document.querySelectorAll("button");
+// const resultH1=document.querySelector("h1");
+// const plusButton=document.querySelector("button");
+// let conut=0;
+// plusButton.addEventListener("click", plus);
+// function plus(){
+//     conut++;
+//     // conut+=1;
+//     // conut+=conut+1;
+//     resultH1.innerHTMl=conut;
+// }
+// plusButton.addEventListener("click", ()=> {
+//     conut++;
+//     resultH1.innerHTMl=conut;
+// });
+// plusButton.onclick=()=>{
+//     conut++;
+//     resultH1.innerHTMl=conut;
+// }
+// const plus=()=>{
+//     conut++;
+//     resultH1.innerHTML=conut;
+// }
