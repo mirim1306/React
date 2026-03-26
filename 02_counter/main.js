bd94ad1d68006e677fd3657d8964417c0365a63b
hwan <kimtaehwanlove1@gmail.com>
2026-03-26 09:19:23 +0900

    02_counter

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,8 +1,8 @@
-let num=0;
-function plus(){
-    num++;
-    document.getElementById("number").innerText=num;
-}
+let count=0;
+// function plus(){
+//     count++;
+//     document.getElementById("count").innerText=count;
+// }
 // const resultH1=document.getElementsByTagName("h1")[0];
 // const plusButton=document.getElementsByTagName("button");
 // const resltH1=document.querySelectorAll("h1")[0];
@@ -11,9 +11,8 @@ function plus(){
 // const plusButton=document.querySelector("button");
 const resultH1=document.getElementById("result");
 // const resultH1=document.querySelector("#result");
-const plusButton=document.getElementById("plusButton");
+// const plusButton=document.getElementById("plusButton");
 // const plusButton=document.querySelector("plusButton");
-let conut=0;
 // plusButton.addEventListener("click", plus);
 // function plus(){
 //     conut++;
@@ -25,11 +24,16 @@ let conut=0;
 //     conut++;
 //     resultH1.innerHTMl=conut;
 // });
-plusButton.onclick=()=>{
-    conut++;
-    resultH1.innerHTMl=conut;
-}
+// plusButton.onclick=()=>{
+//     conut++;
+//     resultH1.innerHTMl=conut;
+// }
 // const plus=()=>{
 //     conut++;
 //     resultH1.innerHTML=conut;
 // }
+resultH1.innerHTML=conut;
+function plus(){
+    conut++;
+    resultH1.innerHTML=conut;
+}
